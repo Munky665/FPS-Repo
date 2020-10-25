af54218e524cddff253061a45ed32377270bf633
Munky665 <adam.b.whitty@gmail.com>
2020-10-25 13:33:11 +1100

    Fixed bug where UI would appear in main menu by creating C# script HideInMenu and attaching it to the parent game object of the UI elements that were appearing in the main menu

---
+++ Assets/Physics/Scripts/HideInMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HideInMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            this.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            this.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+}
